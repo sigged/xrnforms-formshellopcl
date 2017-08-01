360fc40fcc5902dffdd325132eee3937a950f6fa
sigged <sderdeyn@gmail.com>
2017-08-01 15:50:30 +0200

    Added ActionSheet example

--- XrnCourse.FormsHello/XrnCourse.FormsHello/MainPage.xaml.cs
+++ XrnCourse.FormsHello/XrnCourse.FormsHello/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace XrnCourse.FormsHello
 {
@@ -18,5 +20,24 @@ namespace XrnCourse.FormsHello
             await Navigation.PushAsync(new AlertPage());
         }
 
+        private async void btnActionSheet_Clicked(object sender, System.EventArgs e)
+        {
+            string action = await DisplayActionSheet("ActionSheet: What to do?", 
+                "Cancel", null, "Show me the time", "Tell a joke", "Wait 3 seconds");
+
+            if (action == "Show me the time")
+                await DisplayAlert("Time",
+                        $"It's now {DateTime.Now.ToString("HH:mm")},Almost beer-o-clock!", 
+                        "OK");
+            else if (action == "Tell a joke")
+                await DisplayAlert("Joke", 
+                        $"Why is six afraid of seven?{Environment.NewLine}Because seven ate nine.", 
+                        "LOL", "I don't get it");
+            else if (action == "Wait 3 seconds")
+            {
+                await Task.Delay(3000);
+                await DisplayAlert("Wait 3 seconds", "The wait is over.", "Ok");
+            }
+        }
     }
 }
